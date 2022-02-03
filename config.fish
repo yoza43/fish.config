d8428ba4c3512f7f6a64302c4ef1e8388504303d
yoza43 <98686723+yoza43@users.noreply.github.com>
2022-02-03 11:02:18 +0900

    Update config.fish

--- config.fish
+++ config.fish
@@ -13,10 +13,3 @@ end
 #set --export PYENV_ROOT "$HOME/.pyenv"
 #set --export PATH "$PYENV_ROOT/bin:$PATH"
 #eval (pyenv init - | source)
-
-#set --export ftp_proxy http://ac174606:BlestaSESE114514@cproxy.okinawa-ct.ac.jp:8080
-#set --export http_proxy http://ac174606:BlestaSESE114514@cproxy.okinawa-ct.ac.jp:8080
-#set --export https_proxy http://ac174606:BlestaSESE114514@cproxy.okinawa-ct.ac.jp:8080
-#set --export FTP_PROXY http://ac174606:BlestaSESE114514@cproxy.okinawa-ct.ac.jp:8080
-#set --export HTTP_PROXY http://ac174606:BlestaSESE114514@cproxy.okinawa-ct.ac.jp:8080
-#set --export HTTPS_PROXY http://ac174606:BlestaSESE114514@cproxy.okinawa-ct.ac.jp:8080
